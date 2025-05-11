463874937fd259e685ceb79ed814bd69ae6f803e
yokobuto <info@yokobuto.de>
2025-05-11 10:08:50 +0200

    Button with animation added

--- Like Button Animation/ContentView.swift
+++ Like Button Animation/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isLiked = false
+    
     var body: some View {
         VStack {
 
+            Button(
+                action: {
+                    withAnimation{
+                        isLiked.toggle()
+                    }
+                }
+            ){
+                Image(systemName: isLiked ? "heart.fill" : "heart").foregroundColor(isLiked ? .red : .gray)
+            }.scaleEffect(isLiked ? 1.3 : 1.0)
             
             
             
